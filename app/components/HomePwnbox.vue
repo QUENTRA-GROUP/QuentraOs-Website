ef85ae3e5872487c7a2995f6dcae200debc3a71a
MeViksry <kingvikvik25@gmail.com>
2026-08-13 16:34:20 +0700

    style: update Pwnbox layout by wrapping content and adjusting flexbox properties for better alignment

--- app/components/HomePwnbox.vue
+++ app/components/HomePwnbox.vue
@@ -6,8 +6,10 @@
                     <svg viewBox="0 0 24 24" fill="#9BE000" xmlns="http://www.w3.org/2000/svg" class="htb-icon"><path d="M12 2L2 7.77v8.46L12 22l10-5.77V7.77L12 2zm0 2.31l7.6 4.38-3.8 2.19L12 8.7l-3.8 2.18-3.8-2.19L12 4.31zM4 9.92l3.8 2.19v4.38L4 14.31V9.92zm9 6.57l-3.8-2.19v-4.38l3.8-2.18v8.75zm1-8.75l3.8 2.18-3.8 2.19v-4.37zm1 6.56v-4.38l3.8-2.19v4.39l-3.8 2.18z"/></svg>
                     <span class="htb-text">HACKTHEBOX</span>
                 </div>
-                <h2>QuentraOs OS is now <span>web-based</span></h2>
-                <p class="pwnbox-lead">Pwnbox is a completely browser accessible virtual hacking distro including everything a hacker’s operating system should have.</p>
+                <div>
+                    <h2>QuentraOs OS is now <span>web-based</span></h2>
+                    <p class="pwnbox-lead">Pwnbox is a completely browser accessible virtual hacking distro including everything a hacker’s operating system should have.</p>
+                </div>
             </div>
             <div class="pwnbox-image"></div>
         </div>
